dbc2ca10c9369abfdd214a73514430cf3824a6a2
francois <fbeetge@gmail.com>
2022-09-17 17:28:00 +1200

    basic layout

--- thing.js
+++ thing.js
@@ -1,34 +1,51 @@
 #!/usr/bin/env node
-import pkg from 'blessed';
-const { screen: _screen, box: _box} = pkg;
+import blessed from 'blessed';
 import chalk from 'chalk';
 import BlessedContrib from 'blessed-contrib';
 import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
+import XTerm from 'blessed-xterm'
 
-var thing = chalk.blue('Hello') + ' World' + chalk.red('!')
-// Create a screen object.
-var screen = _screen({
-  fastCSR: true
 
+
+var body = 
+chalk.magenta("hsafudhjsdfh\n\rbjksfdhbfsadbhks\n\rfadkhjbfsdhkbjsfadkhbj\n\rsabfkhdaksbhjfdhkjbsa")
+
+const screen = blessed.screen({
+  fastCSR: true,
+  dockBorders: true
 });
+var thing = chalk.blue('Hello') + ' World' + chalk.red('!') 
+var grid = new BlessedContrib.grid({rows: 12, cols: 12, screen: screen})
+
+
+
  
 screen.title = 'my window title';
  
-// Create a box perfectly centered horizontally and vertically.
-var box = _box({
-  bottom:0,
-  left:'center',
-  width: '50%',
-  height: '20%',
-  content: thing,
+// var box = grid.set(7,6,5,6,blessed.box,{
+//   content: thing,
+//   tags: true,
+//   border: {
+//     type: 'line'
+//   },
+//   style: {
+//     border: {
+//       fg: '#f0f0f0'
+//     },
+//     hover: {
+//       bg: 'green'
+//     }
+//   }
+// });
+ 
+var logs = grid.set(6,6,6,6,blessed.box,{
   tags: true,
+  label: 'log',
   border: {
     type: 'line'
   },
   style: {
-    // fg: 'white',
-    // bg: 'magenta',
     border: {
       fg: '#f0f0f0'
     },
@@ -37,12 +54,49 @@ var box = _box({
     }
   }
 });
- 
-// Append our box to the screen.
-screen.append(box);
- 
 
+var terms= grid.set(0,0,12,6, blessed.terminal,{ //more usefull for writing ascii
+  parent: screen,
+  border: 'line',
+  tags: true,
+  scrollable: true,
+  label: '{bold}screen{/bold}',
+  handler: function() {},
+});
 
+var stats=grid.set(0,9,6,1,blessed.box,{
+  tags: true,
+  scrollable: true,
+  label: '{bold}stats{/bold}',
+  content: thing,
+  border: {
+    type: 'line'
+  },
+  style: {
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }})
+
+  var actions=grid.set(0,10,6,2,blessed.list,{
+    tags: true,
+    scrollable: true,
+    label: '{bold}actions{/bold}',
+    content: thing,
+    border: {
+      type: 'line'
+    },
+    style: {
+      border: {
+        fg: '#f0f0f0'
+      },
+      hover: {
+        bg: 'green'
+      }
+    }})
 
 
 
@@ -53,8 +107,48 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
  
-// Focus our element.
-box.focus();
- 
 // Render the screen.
 screen.render();
+
+function rainbow_frame(){
+  terms.term.reset()
+  //frame=rainbow.frame()
+  terms.term.write(body)
+  screen.render()
+}
+//setInterval(rainbow_frame,40)
+
+
+
+var form_thing=grid.set(0,6,6,3,blessed.form,{
+  tags: true,
+  scrollable: true,
+  label: '{bold}choices{/bold}',
+  border: {
+    type: 'line'
+  },
+  style: {
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }})
+
+
+
+
+
+
+
+
+
+const hint = "\r\nPress CTRL+q to stop sample program.\r\n"
+
+//terminal.write(frame)
+
+
+screen.append(terms)
+screen.render()
+

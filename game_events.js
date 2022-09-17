ad1021488b85b264a7c023011895e90e0f49c538
francois <fbeetge@gmail.com>
2022-09-17 19:42:38 +1200

    scrollable logs box

--- game_events.js
+++ game_events.js
@@ -1,5 +1,10 @@
-class game_event{
-    contructor({id,body,buttons}={id:null,body:"",buttons:{}}){
+
+
+
+
+
+export class game_event{
+    constructor(id=null,body,buttons){
         this.id=id
         this.body=body
         this.buttons=buttons
@@ -7,15 +12,15 @@ class game_event{
     }
 }
 
-class game_event_gain_item extends game_event{
-    contructor({id,body,buttons,arryItems}){
+export class game_event_gain_item extends game_event{
+    constructor(id=null,body,buttons,arryItems){
         super(id,body,buttons)
         this.arryItems=arryItems;
     }
 }
 
-class game_event_enemy extends game_event{
-    contructor({id,body,buttons,arryenemy}){
+export class game_event_enemy extends game_event{
+    constructor(id=null,body,buttons,arryenemy){
         super(id,body,buttons)
         this.arryenemy=arryenemy;
     }
--- thing.js
+++ thing.js
@@ -4,16 +4,17 @@ import chalk from 'chalk';
 import BlessedContrib from 'blessed-contrib';
 import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
+import {game_event} from './game_events.js'
 //import XTerm from 'blessed-xterm'
 
 
 
 var body = 
-chalk.magenta("hsafudhjsdfh\n\rbjksfdhbfsadbhks\n\rfadkhjbfsdhkbjsfadkhbj\n\rsabfkhdaksbhjfdhkjbsa")
+chalk.magenta("hsafudhjsdfh\n\rbjksfdhbfsadbhks\n\rfadkhjbfsdhkbjsfadkhbjh\n\rbjksfdhbfsadbhks\n\rfadkhjbfsdhkbjsfadkhbjh\n\rbjksfdhbfsadbhks\n\rfadkhjbfsdhkbjsfadkhbj\n\rsabfkhdaksbhjfdhkjbsa apples")
 
 const screen = blessed.screen({
   fastCSR: true,
-  dockBorders: true
+  dockBorders: true,
 });
 var thing = chalk.blue('Hello') + ' World' + chalk.red('!') 
 var grid = new BlessedContrib.grid({rows: 12, cols: 12, screen: screen})
@@ -26,6 +27,19 @@ screen.title = 'my window title';
 var logs = grid.set(6,6,6,6,blessed.box,{
   tags: true,
   label: 'log',
+  alwaysScroll: 'true',
+  scrollable: 'true',
+  scrollbars: 'true',
+  scrollbar: {
+    ch: ' ',
+    track: {
+      bg: 'blue'
+    },
+    style: {
+      inverse: true
+    }
+  },
+  keys: true,
   border: {
     type: 'line'
   },
@@ -52,7 +66,7 @@ var stats=grid.set(0,9,6,1,blessed.box,{
   tags: true,
   scrollable: true,
   label: '{bold}stats{/bold}',
-  content: thing,
+  content: 'str:4\ncon:5\ndex:4\n',
   border: {
     type: 'line'
   },
@@ -93,21 +107,17 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
  
 
 
-var form_thing=grid.set(0,6,6,3,blessed.form,{
-  tags: true,
-  scrollable: true,
-  label: '{bold}choices{/bold}',
-  border: {
-    type: 'line'
-  },
-  style: {
-    border: {
-      fg: '#f0f0f0'
-    },
-    hover: {
-      bg: 'green'
-    }
-  }})
+var form_thing=grid.set(0,6,6,3,blessed.form = blessed.form,({
+  parent: screen,
+  keys: true,
+  label: "actions",
+  // left: 0,
+  // top: 0,
+  // width: 30,
+  // height: 4,
+  bg: 'magenta',
+  content: 'test?'
+}));
 
 screen.render()
 
@@ -115,9 +125,91 @@ screen.render()
 // multiple functions, exuction may differ based on event type
 
 
+var temp_event1=new game_event(1,body,{1:'goto 1(recursive)',2:'goto 2',3:'goto 3'})
 
 
+var temp_event2=new game_event(2,chalk.blue("event2"),{1:"goto 1"})
+var temp_event2=new game_event({id:3,body:chalk.blue("event3"),buttons:{2:"goto 2"}})
 
 
+var submit = blessed.button({
+  parent: form_thing,
+  mouse: true,
+  keys: true,
+  shrink: true,
+  padding: {
+    left: 1,
+    right: 1
+  },
+  left: 10,
+  top: 2,
+  shrink: true,
+  name: 'submit',
+  content: 'submit',
+  style: {
+    bg: 'blue',
+    focus: {
+      bg: 'red'
+    },
+    hover: {
+      bg: 'red'
+    }
+  }
+});
+
+var cancel = blessed.button({
+  parent: form_thing,
+  mouse: true,
+  keys: true,
+  shrink: true,
+  padding: {
+    left: 1,
+    right: 1
+  },
+  left: 20,
+  top: 2,
+  shrink: true,
+  name: 'cancel',
+  content: 'cancel',
+  style: {
+    bg: 'blue',
+    focus: {
+      bg: 'red'
+    },
+    hover: {
+      bg: 'red'
+    }
+  }
+});
+
+submit.on('press', function() {
+  form_thing.submit();
+});
+
+cancel.on('press', function() {
+  form_thing.reset();
+});
+
+form_thing.on('submit', function(data) {
+  form_thing.setContent('Submitted.');
+  logs.setContent(temp_event1.body)
+  screen.render();
+  logs.focus()
+});
+
+form_thing.on('reset', function(data) {
+  form_thing.setContent('Canceled.');
+  screen.render();
+});
+
+screen.key('q', function() {
+  process.exit(0);
+});
 
 // handling creating of buttons from an event. writing body etc
+form_thing.focus()
+
+
+
+
+//setInterval(function () {console.log(temp_event1.body)}, 10);

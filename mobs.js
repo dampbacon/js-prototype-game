985a77d0ec798a60feb7257572d84ce86926a1f6
Francois Beetge <7982855+dampbacon@users.noreply.github.com>
2022-10-09 04:51:34 +1300

    attack methods for mob

--- mobs.js
+++ mobs.js
@@ -1,19 +1,31 @@
 
 import Chance from 'chance';
+import { Player } from './player';
 const chance1 = new Chance();
 //make roll initiative function in main file
 
 class monster {
-    constructor(name = '', hitDie = 2, AC = 10, aggro/*12always hostile 0 inverse */, morale, weapon, dmgDie, rarity) {
+    constructor(name = '', hitDie = 2, ac = 10, aggro/*12always hostile 0 inverse */, morale, weapon, dmgDie, rarity) {
         this.name = name;
         this.hitDie = hitDie;
-        this.AC = AC;
+        this.ac = ac;
         this.morale = morale;
         this.weapon = weapon;
         this.dmgDie = dmgDie;
         this.aggro = aggro;
         this.rarity = rarity;
         this.hp = chance1.rpg(`${this.hitDie}d6`, { sum: true });
+    }
+    attack(target=new(Player)){
+        if (this.#rollToHit() >= target.ac) {
+            target.decreaseHP(this.#rollDamage())
+        }
+    }
+    #rollDamage() {
+        return chance1.rpg(this.dmgDie, { sum: true })
+    }
+    #rollToHit() {
+        return chance1.rpg(this.weapon, { sum: true }) 
     }
 
 }
